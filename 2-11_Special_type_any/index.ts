317c6762861072ae1c6355315ba0149af1bb544b
Taiga <taiga507@gmail.com>
2024-04-27 16:45:12 +0400

    2-11
    Special type any

---
+++ 2-11_Special_type_any/index.ts
@@ -0,0 +1,13 @@
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "Jhon"}';
+
+const userObj: {
+	isBirthdayData: boolean;
+	ageDataage: number;
+	userNameData: string;
+} = JSON.parse(userData);
+
+let salary;
+salary = 5000;
+
+let salary: number;
+salary = 5000;
