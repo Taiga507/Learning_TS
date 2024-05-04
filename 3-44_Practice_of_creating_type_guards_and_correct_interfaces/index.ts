13ebaf21deda457f31873f73883fc0bc99ea18b5
Taiga <taiga507@gmail.com>
2024-05-04 17:11:32 +0400

    3-44
    Practice of creating type guards and correct interfaces

--- 3-44_Practice_of_creating_type_guards_and_correct_interfaces/index.ts
+++ 3-44_Practice_of_creating_type_guards_and_correct_interfaces/index.ts
@@ -4,9 +4,15 @@
 //     breed: string,
 //     sterilized?: string
 // }
+type Animals = 'cat' | 'dog' | 'bird';
+
+interface Animal {
+	animal: Animals;
+	breed: string;
+	sterilized?: string;
+};
 
 // Response #1
-
 // {
 //     status: 'available',
 //     data: {
@@ -17,9 +23,22 @@
 //         age?: number
 //     }
 // }
+enum AnimalsStatus {
+	AVAILABLE = 'available',
+	NOTAVAILABLE = 'not available'
+};
+
+interface ResponseTrueData extends Animal {
+	location: string;
+	age?: number;
+};
+
+interface ResponseTrue {
+	status: AnimalsStatus.AVAILABLE
+	data: ResponseTrueData;
+};
 
 // Response #2
-
 // {
 //     status: 'not available',
 //     data: {
@@ -27,12 +46,40 @@
 //         nextUpdateIn: Date
 //     }
 // }
+interface ResponseFalseData {
+	message: string;
+	nextUpdateIn: Date;
+};
 
-function checkAnimalData(animal) {
-	if ("available") {
-		// Заменить условие!
+interface ResponseFalse {
+	status: AnimalsStatus.NOTAVAILABLE
+	data: ResponseFalseData;
+};
+
+type Res = ResponseTrue | ResponseFalse;
+
+function isAvailable(res: Res): res is ResponseTrue {
+	if (res.status === AnimalsStatus.AVAILABLE) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+function checkAnimalData(animal: Res): string | ResponseTrueData {
+	if (isAvailable(animal)) {
 		return animal.data;
 	} else {
-		return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`;
+		return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
 	}
 }
+
+const iriska: ResponseFalse = {
+	status: AnimalsStatus.NOTAVAILABLE,
+    data: {
+        message: 'uuups',
+		nextUpdateIn: new Date(2025/12/12)
+    }
+}
+
+console.log(checkAnimalData(iriska));
