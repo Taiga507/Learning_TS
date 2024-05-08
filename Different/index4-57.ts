390adda3ede8ab023faea6b95d6ce2e9d47bb3cf
Taiga <taiga507@gmail.com>
2024-05-08 21:17:29 +0400

    4-59
    Practice

---
+++ Different/index4-57.ts
@@ -0,0 +1,33 @@
+function printMsg(msg: string[] | number | boolean): void {
+	if (Array.isArray(msg)) {
+		msg.forEach((m) => console.log(m));
+	} else if (typeof msg === 'number') {
+		console.log(msg);
+	} else {
+		console.log(msg);
+	}
+	console.log(msg);
+}
+
+
+const dataFromControl = {
+	water: 200,
+	el: 500
+};
+
+function chechReadings(data: typeof dataFromControl): boolean {
+	const dataFromUser = {
+		water: 200,
+		el: 500
+	};
+	// ...
+};
+
+
+const google = {
+	name: 'Google',
+	debts: 50000
+};
+
+type GoogleKeys = keyof google; // Error! google is an object, not a type
+type GoogleKeys = keyof typeof google; // 'name' | 'debts'
