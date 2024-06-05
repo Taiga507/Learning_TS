73dd8b4928fea3e311f99b730f1a063b2f342efa
Taiga <taiga507@gmail.com>
2024-06-05 17:37:50 +0400

    7-111/112 Beauty
    Working with time / added spiner, error, states

--- Beauty/src/components/appointmentList/AppointmentList.tsx
+++ Beauty/src/components/appointmentList/AppointmentList.tsx
@@ -1,15 +1,33 @@
 import { useContext, useEffect } from "react";
 
 import AppointmentItem from "../appointmentItem.tsx/AppointmentItem";
+import Spinner from "../spinner/Spinner";
+import Error from "../error/Error";
 import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
 
 function AppointmentList() {
-	const { activeAppointments, getActiveAppointments } = useContext(AppointmentContext);
+	const {
+		activeAppointments,
+		getActiveAppointments,
+		appointmentLoadingStatus,
+	} = useContext(AppointmentContext);
 
 	useEffect(() => {
 		getActiveAppointments();
-	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
+	if (appointmentLoadingStatus === "loading") {
+		return <Spinner />;
+	} else if (appointmentLoadingStatus === "error") {
+		return (
+			<>
+				<Error />
+				<button className="schedule__reload" onClick={getActiveAppointments}>
+					Try to reload
+				</button>
+			</>
+		);
+	}
 
 	return (
 		<>
