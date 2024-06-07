a3a940afc326ee74f614e545db5c4e5120de2090
Taiga <taiga507@gmail.com>
2024-06-08 00:38:30 +0400

    7-119 Beauty
    Generation of new records

--- Beauty/src/services/AppointmentService.ts
+++ Beauty/src/services/AppointmentService.ts
@@ -1,11 +1,13 @@
-import { useHttp } from "../hooks/http.hook";
-import hasRequiredFields from "../utils/hasRequiredFields";
-import dayjs from "dayjs";
-
 import {
 	IAppointment,
 	ActiveAppointment,
 } from "../shared/interfaces/appointment.interface";
+import { useHttp } from "../hooks/http.hook";
+import hasRequiredFields from "../utils/hasRequiredFields";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
+dayjs.extend(customParseFormat);
 
 const requiredFields = ["id", "date", "name", "service", "phone", "canceled"];
 
@@ -51,11 +53,25 @@ const useAppointmentService = () => {
 		});
 	};
 
+	const createNewAppointment = async (body: IAppointment) => {
+		const id = new Date().getTime();
+		body["id"] = id;
+		body["date"] = dayjs(body.date, "DD/MM/YYYY HH:mm").format(
+			"YYYY-MM-DDTHH:mm"
+		);
+		return await request({
+			url: _apiBase,
+			method: "POST",
+			body: JSON.stringify(body),
+		});
+	};
+
 	return {
 		loadingStatus,
 		getAllAppointments,
 		getAllActiveAppointments,
 		cancelOneAppointment,
+		createNewAppointment,
 	};
 };
 
